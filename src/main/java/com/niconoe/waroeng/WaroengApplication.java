7e71baa7e71431f3da1e0e6d32c53cd815ac3f32
dody.setiawan <setiawandody2003@gmail.com>
2022-04-21 20:40:57 +0700

    init

---
+++ src/main/java/com/niconoe/waroeng/WaroengApplication.java
@@ -0,0 +1,13 @@
+package com.niconoe.waroeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaroengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaroengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niconoe/waroeng/WaroengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niconoe.waroeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaroengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
